1dcdd272d3eb5f4df6e18faf444145a9e022b7cb
tin-snowflake <cqtin0903@gmail.com>
2024-05-22 17:05:46 +0800

    fix doc

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,9 @@
  * player.rs
  * - Players need to have keypairs - their own secret and public keys.
  * - When a player or players draw a card, we need to choose an input for the VRFs of players who draw cards.
+ * - Random number generation
  * - One good way to get an input is for all players to do a commit-reveal and combine the results,
  * however you could choose whatever technique you'd like.
- * - Random number generation
  *
  * vrf.rs
  * - Players know their own VRF output (i.e. the cards in their hand),
@@ -12,4 +12,3 @@
  */
 
 fn main() {}
-
