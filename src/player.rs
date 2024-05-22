1737a8cc15ae50b0ce1377d8362acf914f4c53ce
Omar Carey <omarcarey@Omars-MacBook-Pro-2.local>
2024-05-22 17:35:48 +0800

    add draw method

--- src/player.rs
+++ src/player.rs
@@ -1,21 +1,36 @@
-use schnorrkel::{Keypair, PublicKey, SecretKey};
+use schnorrkel::{
+    context::signing_context,
+    vrf::{VRFInOut, VRFProof},
+    Keypair, PublicKey, SecretKey,
+};
 
-pub struct Player(Keypair);
+pub struct Player {
+    key_pair: Keypair,
+    hand: Option<VRFInOut>,
+}
 
 impl Player {
     pub fn new() -> Self {
-        Self(Keypair::generate())
-    }
-
-    pub fn keypair(&self) -> &Keypair {
-        &self.0
+        Self {
+            key_pair: Keypair::generate(),
+            hand: None,
+        }
     }
 
     pub fn key_private(&self) -> &SecretKey {
-        &self.0.secret
+        &self.key_pair.secret
     }
 
     pub fn key_public(&self) -> &PublicKey {
-        &self.0.public
+        &self.key_pair.public
+    }
+
+    pub fn draw(&mut self, input: &[u8]) -> VRFProof {
+        let ctx = signing_context(b"yo!");
+        let (output, signature, _) = self.key_pair.vrf_sign(ctx.bytes(input));
+
+        self.hand = Some(output);
+
+        signature
     }
 }
