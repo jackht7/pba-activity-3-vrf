c6e1b2197ab4ef43a494b13b79c912a25b36e37a
tin-snowflake <cqtin0903@gmail.com>
2024-05-22 19:26:54 +0800

    fix u32

--- src/player.rs
+++ src/player.rs
@@ -11,6 +11,13 @@ use schnorrkel::{
 #[derive(Clone)]
 pub struct Player(Keypair);
 
+fn as_u32_be(array: &[u8; 32]) -> u32 {
+    ((array[0] as u32) << 24)
+        + ((array[1] as u32) << 16)
+        + ((array[2] as u32) << 8)
+        + ((array[3] as u32) << 0)
+}
+
 impl Player {
     pub fn new() -> Self {
         Self(Keypair::generate())
@@ -23,10 +30,10 @@ impl Player {
     pub fn draw(&self, input: &[u8]) -> (u32, Transcript, VRFInOut, VRFProof) {
         let ctx = signing_context(b"Drawing card");
         let transcript = ctx.bytes(input);
-        let (output, signature, _) = self.0.vrf_sign(transcript);
+        let (output, signature, _) = self.0.vrf_sign(transcript.clone());
 
         let output_byte = output.as_output_bytes();
-        let card = from_utf8(output_byte).unwrap().parse::<u32>().unwrap() % 13;
+        let card = as_u32_be(output_byte) % 13;
 
         (card, transcript, output, signature)
     }
