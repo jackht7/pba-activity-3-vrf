07bf03d8fbc4f7aa70d8fcf05065883afe7d1a0c
Omar Carey <omarcarey@Omars-MacBook-Pro-2.local>
2024-05-22 18:14:53 +0800

    update draw to validate on draw

--- src/player.rs
+++ src/player.rs
@@ -1,6 +1,6 @@
 use std::str::from_utf8;
 
-use schnorrkel::{context::signing_context, vrf::VRFProof, Keypair, PublicKey, SecretKey};
+use schnorrkel::{context::signing_context, errors::SignatureError, Keypair, PublicKey, SecretKey};
 
 const DEFAULT_BALANCE: u32 = 100;
 
@@ -27,15 +27,22 @@ impl Player {
         &self.key_pair.public
     }
 
-    pub fn draw(&mut self, input: &[u8]) -> VRFProof {
+    pub fn draw(&mut self, input: &[u8]) -> Result<(), SignatureError> {
         let ctx = signing_context(b"yo!");
-        let (output, signature, _) = self.key_pair.vrf_sign(ctx.bytes(input));
+        let transcript = ctx.bytes(input);
+        let (output, signature, _) = self.key_pair.vrf_sign(transcript.clone());
 
-        let output_byte = output.as_output_bytes();
+        let verified_output =
+            self.key_public()
+                .vrf_verify(transcript, &output.to_preout(), &signature)?;
+
+        let output_byte = verified_output.0.as_output_bytes();
         let hand_index = from_utf8(output_byte).unwrap().parse::<u32>().unwrap() % 13;
 
         self.hand = Some(hand_index);
 
-        signature
+        Ok(())
     }
+
+    pub fn validate(&self) {}
 }
